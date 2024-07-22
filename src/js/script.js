3774f97887c128be0397ec64318218d039efe082
miracle <example@gmail.com>
2024-07-22 12:42:35 +0200

    media 320

--- src/js/script.js
+++ src/js/script.js
@@ -8,10 +8,53 @@ $(document).ready(function () {
       {
         breakpoint: 992,
         settings: {
-          dots: true,
+          dots: false,
           arrows: false,
+          centerMode: true,
+          variableWidth: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: true,
+          variableWidth: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
     ]
   });
+
+  // скрипт для табов. ласт строка для контента
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
 });
