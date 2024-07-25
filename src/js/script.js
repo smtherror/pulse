436b69419dd78a80eb95609da96b7567f0ae37dc
dimka <example@gmail.com>
2024-07-25 20:28:37 +0200

    model

--- src/js/script.js
+++ src/js/script.js
@@ -1,65 +1,82 @@
 $(document).ready(function () {
-  $('.carousel__inner').slick({
-    speed: 1200,
-    // dots: true,
-    // adaptiveHeight: true,
-    prevArrow: '<button type="button" class="slick-prev"> <img src="../img/arrow_left.svg"> </button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow_right.svg"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          arrows: false,
-          centerMode: true,
-          variableWidth: true
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          dots: true,
-          arrows: false,
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          centerMode: true,
-          variableWidth: true
-        }
-      },
-      {
-        breakpoint: 575,
-        settings: {
-          arrows: false,
-          dots: true,
-          centerMode: true,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
+	$('.carousel__inner').slick({
+		speed: 1200,
+		// dots: true,
+		// adaptiveHeight: true,
+		prevArrow: '<button type="button" class="slick-prev"> <img src="../img/arrow_left.svg"> </button>',
+		nextArrow: '<button type="button" class="slick-next"><img src="../img/arrow_right.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					dots: true,
+					arrows: false,
+					centerMode: true,
+					variableWidth: true
+				}
+			},
+			{
+				breakpoint: 767,
+				settings: {
+					dots: true,
+					arrows: false,
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					centerMode: true,
+					variableWidth: true
+				}
+			},
+			{
+				breakpoint: 575,
+				settings: {
+					arrows: false,
+					dots: true,
+					centerMode: true,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
 
-  // скрипт для табов. ласт строка для контента
+	// скрипт для табов. ласт строка для контента
 
-  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
-    $(this)
-      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
-  });
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
 
-  function toggleSlide(item) {
-    $(item).each(function (i) {
-      $(this).on('click', function (e) {
-        e.preventDefault();
-        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-      })
-    });
-  };
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+			})
+		});
+	};
 
-  toggleSlide('.catalog-item__link');
-  toggleSlide('.catalog-item__back');
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+
+	// Modal
+
+	$('[data-modal=consultation]').on('click', function () {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function () {
+		$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+	});
+
+
+	$('.button_mini').each(function (i) {
+		$(this).on('click', function () {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		})
+	});
 
 });
