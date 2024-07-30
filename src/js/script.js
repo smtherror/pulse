680cfe5ad36ada4603b0e592cea5216720bd9b88
dimka <example@gmail.com>
2024-07-30 14:57:15 +0200

    mailer

--- src/js/script.js
+++ src/js/script.js
@@ -79,4 +79,67 @@ $(document).ready(function () {
 		})
 	});
 
+
+	// Валидация
+
+
+
+
+	$('#order form').validate();
+
+	function validateForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: 'required',
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Минимум {0} символа!")
+				},
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+					required: "Пожалуйста, введите свой email",
+					email: "Your email address must be in the format of name@domain.com"
+				}
+			}
+		});
+	}
+
+	validateForms('#consultation-form');
+	validateForms('#consultation form');
+	validateForms('#order form');
+
+
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function (e) {
+		e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function () {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
 });
